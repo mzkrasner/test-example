d2108602e6920295d1b7bdfdec321a0286de27a9
khartig <kevin@fiseportal.com>
2023-07-27 11:40:40 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import {Ed25519Provider} from 'key-did-provider-ed25519'
+import KeyResolver from 'key-did-resolver'
+import {DID} from 'dids'
+
+import seedrandom from 'seedrandom'
+
+const App = () => {
+    const [randomDid, setRandomDid] = useState('')
+    const [staticDid, setStaticDid] = useState('')
+
+    const createRandomDID = async () => {
+        const seed = new Uint8Array(32)
+        crypto.getRandomValues(seed)
+        const randomDid = new DID({
+            provider: new Ed25519Provider(seed),
+            resolver: KeyResolver.getResolver()
+        })
+        await randomDid.authenticate()
+
+        setRandomDid(randomDid.id)
+    };
+
+    const createStaticDID = async () => {
+        const uniqueKey = '0x7a8b9cde'
+        const rng = seedrandom(uniqueKey)
+        const seed = new Uint8Array(32)
+        for (let i = 0; i < 32; i += 1) {
+            seed[i] = Math.floor(rng() * 256)
+        }
+        const staticDid = new DID({
+            provider: new Ed25519Provider(seed),
+            resolver: KeyResolver.getResolver()
+        })
+        await staticDid.authenticate()
+
+        setStaticDid(staticDid.id)
+    };
+
+    return (
+        <div>
+            <h1>Create DID</h1>
+            <button onClick={createRandomDID}>Random DID</button>
+            <p>DID: {randomDid}</p>
+            <br/>
+            <button onClick={createStaticDID}>Static DID</button>
+            <p>DID: {staticDid}</p>
+        </div>
+    );
+};
 
 export default App;
